acad81780a62b6bac984d52c33266523cb6fd301
Student <p0tato.polskiziemniaczek@gmail.com>
2019-10-04 12:05:19 +0200

    222

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Lab1 Brzakala");
+        System.out.println("Lab1 Brzakala nowyasdasdasdasd");
 	// write your code here
     }
 }
