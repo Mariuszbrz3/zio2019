279fa591a69dfd540c92958c37cfebdca029fe49
Student <p0tato.polskiziemniaczek@gmail.com>
2019-10-04 12:09:01 +0200

    222

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Lab1 Brzakala trzeci");
         System.out.println("zobaczymy");
+        System.out.println("3232323");
 	// write your code here
     }
 }
